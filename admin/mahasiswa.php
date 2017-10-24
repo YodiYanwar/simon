bd0de061037e02f5ebb3c2ae22176eb3646f5fe2
YodiYanwar <yodi.yanwar@gmail.com>
2017-10-24 15:55:30 +0700

    Tambah Page

---
+++ admin/mahasiswa.php
@@ -0,0 +1,19 @@
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Manajemen Pengguna
+        <small>Mahasiswa</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="/simon/admin"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li class="active">Manajemen Pengguna</li>
+        <li>Mahasiswa</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      
+
+    </section>
+    <!-- /.content -->
--- index.php
+++ index.php
@@ -1,10 +1,83 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Sistem Informasi Monitoring Matrikulasi</title>
-	<link rel="shortcut icon" href="assets/img/favicon.ico" />
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Sistem Informasi Monitoring Matrikulasi | Log in</title>
+        <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+        <!-- Bootstrap 3.3.2 -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" >
+        <!-- Font Awesome Icons -->
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+        <!-- Theme style -->
+        <link href="assets/css/AdminLTE.min.css" rel="stylesheet">  
+        <!-- Favicon -->
+        <link rel="shortcut icon" href="assets/img/favicon.ico" />  
+        <!-- iCheck -->
+        <link rel="stylesheet" href="assets/css/blue.css">        
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="assets/css/ionicons.min.css">  
+        <!-- Google Font -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-</body>
+        <!-- Ionicons -->
+        <link rel="stylesheet" href="assets/css/simon.css">  
+
+
+    </head>
+    <body class="hold-transition login-page">
+        <div class="login-box">
+          <div class="login-logo">
+            <a href="index.php"> <img src="assets/img/icon.png">&nbsp;<b>Simon</b>Matrik</a><br>
+          </div>
+          <!-- /.login-logo -->
+          <div class="login-box-body">
+            <p class="login-box-msg">Sistem Informasi Monitoring<br>Matrikulasi</p>
+
+            <form action="login.php" method="POST">
+              <div class="form-group has-feedback">
+                <input type="text" class="form-control" placeholder="Username" name="username" required autofocus>
+                <span class="fa fa-user form-control-feedback"></span>
+              </div>
+              <div class="form-group has-feedback">
+                <input type="password" class="form-control" placeholder="Password" name="password" required>
+                <span class="fa fa-lock form-control-feedback"></span>
+              </div>
+              <div class="row"><br>
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6">
+                  <button type="submit" class="btn btn-primary btn-block btn-flat" name="login">Login</button>
+                </div>
+                <!-- /.col -->
+              </div>
+            </form>
+            <!--
+            <a href="#">I forgot my password</a><br>
+            <a href="register.html" class="text-center">Register a new membership</a>
+            -->
+          </div>
+          <!-- /.login-box-body -->
+        </div>
+        <!-- /.login-box -->
+
+
+        <!-- jQuery 3 -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- Popper -->
+        <script src="assets/js/popper.min.js"></script>
+        <!-- Bootstrap 3.3.2 JS -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!-- iCheck -->
+        <script src="assets/js/icheck.min.js"></script>        
+
+        <script>
+          $(function () {
+            $('input').iCheck({
+              checkboxClass: 'icheckbox_square-blue',
+              radioClass: 'iradio_square-blue',
+              increaseArea: '20%' // optional
+            });
+          });
+        </script>        
+    </body>
 </html>
