46a97fea5c89539e85328493a497355daa2db8fa
Rahayu Rita <rahayurita134@gmail.com>
2017-10-24 10:16:54 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sistem Informasi Monitoring Matrikulasi</title>
+	<link rel="shortcut icon" href="assets/img/favicon.ico" />
+</head>
+<body>
+
+</body>
+</html>
