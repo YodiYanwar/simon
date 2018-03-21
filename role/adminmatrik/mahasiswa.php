b81628f9e82219da044df7c3251a61823c2010a8
YodiYanwar <yodieys_yanwar@windowslive.com>
2018-03-21 17:32:09 +0700

    change badge color

--- role/adminmatrik/mahasiswa.php
+++ role/adminmatrik/mahasiswa.php
@@ -47,9 +47,9 @@
                    ?>
                 <tr>
                   <td><?php echo $no ?></td>
-                  <td><?php echo "<span class='badge bg-green'>".$row['nim']."</span>" ?></td>
+                  <td><?php echo "<span class='badge'>".$row['nim']."</span>" ?></td>
                   <td><?php echo "<a href='index.php?page=mahasiswadetails&id=".$row['id_user']."'>".$row['nama']."</a>" ?></td>
-                  <td></td>
+                  <td><?php if($row['j_kelamin'] == 'Ikhwan' || $row['j_kelamin'] == 'Laki-laki'){echo '<span class="label bg-green">Ikhwan</span>';} else if($row['j_kelamin'] == 'Akhwat' || $row['j_kelamin'] == 'Perempuan'){echo '<span class="label bg-yellow">Akhwat</span>';} else if($row['j_kelamin'] == NULL){echo '<span class="label bg-gray">Belum diset</span>';} ?></td>
                   <td><?php if ($row['last_login'] == '0000-00-00 00:00:00'){ echo 'Belum Pernah';}else{ echo date("d-m-Y H:i", strtotime($row['last_login'])) ;}
                   ?></td>
                   <td>
