eb321a2adf6525694ae4ac43619822017a6aa171
YodiYanwar <yodieys_yanwar@windowslive.com>
2018-04-18 10:02:48 +0700

    Fixing table pmaindetail

--- role/adminmatrik/pelanggaran/pikhtisar_detail.php
+++ role/adminmatrik/pelanggaran/pikhtisar_detail.php
@@ -20,9 +20,10 @@
     <section class="content">
       <div class="row">
         <div class="col-xs-12">
-          <div class="box">
-            <div class="box-header with-border">
-             <!-- <a href="index.php?page=tambahp" class="btn btn-primary"><i class="fa fa-plus"></i>&nbsp;Tambah</a> -->
+          <div class="box box-primary">
+            <div class="box-header">
+              <h3 class="box-title">Detil Pelanggaran</h3>
+              <!-- <a href="index.php?page=tambahp" class="btn btn-primary"><i class="fa fa-plus"></i>&nbsp;Tambah</a> -->
             </div>
             <!-- /.box-header -->
             <!-- /.box-header -->
